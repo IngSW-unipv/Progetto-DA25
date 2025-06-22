213a28e1e272d29de217ab0fc062aae664b503c0
gorelliglo <gloria.gorelli01@universitadipavia.it>
2025-06-22 23:15:49 +0200

    Update MessaggioBacheca.java

--- TT/src/model/MessaggioBacheca.java
+++ TT/src/model/MessaggioBacheca.java
@@ -7,12 +7,27 @@ public class MessaggioBacheca {
     private String username;
     private Timestamp dataPubblicazione;
 
-    public String getMessaggio() { return messaggio; }
-    public void setMessaggio(String messaggio) { this.messaggio = messaggio; }
+    public String getMessaggio() { 
+        return messaggio; 
+    }
+    
+    public void setMessaggio(String messaggio) { 
+        this.messaggio = messaggio;
+    }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getUsername() { 
+        return username; 
+    }
+    
+    public void setUsername(String username) { 
+        this.username = username; 
+    }
 
-    public Timestamp getDataPubblicazione() { return dataPubblicazione; }
-    public void setDataPubblicazione(Timestamp dataPubblicazione) { this.dataPubblicazione = dataPubblicazione; }
+    public Timestamp getDataPubblicazione() { 
+        return dataPubblicazione;
+    }
+    
+    public void setDataPubblicazione(Timestamp dataPubblicazione) { 
+        this.dataPubblicazione = dataPubblicazione; 
+    }
 }
